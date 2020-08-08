223f64f654b0796820273a7149edd1bbac49a314
practical-trainer-5867 <misterjcr@aol.com>
2020-08-08 03:34:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function appendKitten(name) {
 }
 
 function prependKitten(name) {
-  return [name, ...kittens]
+  return [name, ...kittens];
 }
 function prependKitten() {
   return kittens.slice(0,kittens.length-1)
