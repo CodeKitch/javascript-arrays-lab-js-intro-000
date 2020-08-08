c272471b82ca7874d81739db8f7c9450c4ecb71f
practical-trainer-5867 <misterjcr@aol.com>
2020-08-08 03:42:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ function appendKitten(name) {
 function prependKitten(name) {
   return [name, ...kittens]
 }
-function prependKitten() {
-  return kittens.slice(0 ,kittens.length -1)
+function removeLastKitten() {
+  return kittens.slice(1)
 }
 
-function removeLastKitten() {
+function removeFirstKitten() {
   return kittens.slice(0, array.length - 1)
 }
